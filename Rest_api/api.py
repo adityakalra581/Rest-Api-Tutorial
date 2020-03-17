615c3482f64c62f4ae639558e74d2f1cd5e8b605
adityakalra581 <adityakalra581@gmail.com>
2020-03-17 13:00:57 +0530

    Transactions Model Added

--- Rest_api/api.py
+++ Rest_api/api.py
@@ -207,19 +207,6 @@ def create_order(current_user):
 
     return jsonify({'message' : "Order created!"})
 
-# @app.route('/todo/<todo_id>', methods=['PUT'])
-# @token_required
-# def complete_todo(current_user, todo_id):
-#     todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
-
-#     if not todo:
-#         return jsonify({'message' : 'No todo found!'})
-
-#     todo.complete = True
-#     db.session.commit()
-
-#     return jsonify({'message' : 'Todo item has been completed!'})
-
 @app.route('/order/<order_id>', methods=['DELETE'])
 @token_required
 def delete_order(current_user, order_id):
