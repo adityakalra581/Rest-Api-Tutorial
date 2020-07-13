8af344cd71b9a8b9cb2df99213a9f83bc7d9f073
adityakalra581 <adityakalra581@gmail.com>
2020-07-13 14:03:16 +0530

    Tuples converted into dict

--- list_to_dict.py
+++ list_to_dict.py
@@ -1,13 +1,38 @@
+########
+## TASK: to convert List of Tuples into List of Dictionaries.
+
+
+
+
+## Original Data: List of Tuples.
 r = [('Aditya', 'Elli'), ('Aditya', 'Spot'), ('Aman', None), ('Anthony', 'Brian'), ('Anthony', 'june')]
 
 
-x = {}
+# x = {}
 
-def list_to_dict(r):
-    for result in r:
-        if result[1]:
-            x = {"name":result[0],"pet":result[1]}
-    print(x)
+# def list_to_dict(r):
+#     for result in r:
+#         if result[1]:
+#             x = {"name":result[0],"pet":result[1]}
+    # print(x)
+
+# ************************************************
+## For less number of columns this can work.
+
+keys = ['name','pets']
+final = []
+
+for result in r:
+    for i in range(1):
+        inner_result = {}
+        inner_result['name']  = result[i]
+        inner_result['pets'] = result[i+1]
+    final.append(inner_result)
 
 
-list_to_dict(r)
+print(final)
+
+
+
+
+# list_to_dict(r)
