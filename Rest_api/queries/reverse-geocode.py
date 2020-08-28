0b97614204d4af406fe00db5e2fb8987c8c7b59b
adityakalra581 <adityakalra581@gmail.com>
2020-08-28 13:41:46 +0530

    queries updated

--- Rest_api/queries/reverse-geocode.py
+++ Rest_api/queries/reverse-geocode.py
@@ -7,6 +7,7 @@ geolocator = Nominatim(user_agent='test/1')
 location = geolocator.reverse('28.6995123, 77.1301047')
 print(type(location.address))
 print(location.address)
+print(location.raw)
 
 add = location.address
 
@@ -32,7 +33,24 @@ print(Nominatim(user_agent='test/1').reverse("28.63177831658984, 77.244394179841
 ## Man Singh Road
 
 print(Nominatim(user_agent='test/1').reverse("28.612482152364123,77.22414473180977").address)
+print(Nominatim(user_agent='test/1').reverse("28.612482152364123,77.22414473180977").raw)
 
 ## Noida 
 
-print(Nominatim(user_agent='test/1').reverse("28.58601421313013,77.35868469660956").address)
+print(Nominatim(user_agent='test/1').reverse("28.58601421313013,77.35868469660956").raw)
+
+
+## TASK: to get county value present in address dictionary inside sample dict.
+
+sample = {'place_id': 103767223, 'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright', 'osm_type': 'way',
+          'osm_id': 66188357, 'lat': '28.586119125315207', 'lon': '77.3587932881799',
+           'display_name': 'Sector 34, Noida, Dadri, Gautam Buddha Nagar, Uttar Pradesh, 201301, India', 
+            'address': {'residential': 'Sector 34', 'city': 'Noida', 'county': 'Dadri', 'state_district': 'Gautam Buddha Nagar', 
+                        'state': 'Uttar Pradesh','postcode': '201301', 'country': 'India', 'country_code': 'in'},
+                         'boundingbox': ['28.5825836', '28.5870081', '77.3579537', '77.3622022']}
+
+print(sample['address']['county'])
+
+## Dadri
+
+print((Nominatim(user_agent='test/1').reverse("28.58601421313013,77.35868469660956").raw)['address']['county'])
